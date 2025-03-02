21702292c6776eeda01e208afcf039661b2da9ad
SFRL <sebastian.lobbers@googlemail.com>
2025-03-02 22:26:26 +0000

    use reddit instead of talklife

--- src/App.jsx
+++ src/App.jsx
@@ -4,9 +4,19 @@ import FilterPanel from './filterPanel';
 import { useState, useEffect} from 'react';
 import summaries from './assets/timeline_summarization_orders.json'
 
+const summaryTypes = [
+              "Llama-3.1-8B-Instruct",  
+              "Human",
+            ]
+
 function App() {
-  const [timelineId, setTimelineId] = useState("52568_104")
-  const [summary, setSummary] = useState("Best LLaMA")
+  const [timelineId, setTimelineId] = useState("0cac13e357")
+  const [summary, setSummary] = useState(summaryTypes[0])
+
+  const setInfo = (id, sum) => {
+    setTimelineId(id)
+    setSummary(sum)
+  }
 
   useEffect(() => {
     console.log(summaries[timelineId])
@@ -14,7 +24,11 @@ function App() {
 
   return (
     <div className='is-flex p-2 root-container'>
-      <FilterPanel getTimelineId={setTimelineId} getSummary={setSummary}/>
+      <FilterPanel 
+        setFilter={setInfo}
+        timelineIds={Object.keys(summaries)}
+        summaryTypes={summaryTypes}  
+      />
       <ContentPanel tlid={timelineId} summary={summaries[timelineId][summary]}/>
     </div>
   )
--- src/filterPanel.jsx
+++ src/filterPanel.jsx
@@ -1,13 +1,48 @@
 import Panel from "./components/panel";
+import PropTypes from 'prop-types';
+import { useState} from "react";
 
 
-const FilterPanel = () => {
+const FilterPanel = ({setFilter, timelineIds, summaryTypes}) => {
+    const [timelineId, setTimelineId] = useState(timelineIds[0])
+    const [summary, setSummary] = useState(summaryTypes[0])
+
     return (
         <Panel>
-            <h1>Filter Panel</h1>
-            <p>This is the filter panel</p>
+
+            <h1 className="subtitle is-5">Timeline</h1>
+            <div className="select">
+                <select onChange={(e) => setTimelineId(e.target.value)}>
+                    {
+                        timelineIds.map((id, index) => {
+                            return <option key={index} value={id}>{id}</option>
+                        })
+                    }
+                </select>
+            </div>
+
+            <h1 className="subtitle is-5">Summary</h1>
+            <div className="select">
+                <select onChange={(e) => setSummary(e.target.value)}>
+                    {
+                        summaryTypes.map((type, index) => {
+                            return <option key={index} value={type}>{type}</option>
+                        })
+                    }
+                </select>
+            </div>
+            <button className="button is-primary" 
+                onClick={() => setFilter(timelineId, summary)}>
+                    Generate
+            </button>
         </Panel>
     );
+}
+
+FilterPanel.propTypes = {
+    setFilter: PropTypes.func.isRequired,
+    timelineIds: PropTypes.arrayOf(PropTypes.string).isRequired,
+    summaryTypes: PropTypes.arrayOf(PropTypes.string).isRequired
 }
 
 export default FilterPanel;
